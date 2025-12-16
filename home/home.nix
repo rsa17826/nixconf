b3ce89da1c90c338ce28a334bf54fce35d4a796c
Your Name <you@example.com>
2025-12-16 08:47:15 -0500

    a

--- home/home.nix
+++ home/home.nix
@@ -55,8 +55,8 @@
       plugins = [
         "${pkgs.anyrun}/lib/libapplications.so"
         "${pkgs.anyrun}/lib/libsymbols.so"
-        "${pkgs.anyrun}/lib/libshell.so"
-        "${pkgs.anyrun}/lib/libdictionary.so"
+        #"${pkgs.anyrun}/lib/libshell.so"
+        #"${pkgs.anyrun}/lib/libdictionary.so"
       ];
     };
 
