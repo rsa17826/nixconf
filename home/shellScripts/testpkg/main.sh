336c0d3c5f5034d90612af7747f53ec4ea3fdc47
Your Name <you@example.com>
2026-01-09 16:16:58 -0500

    a

--- home/shellScripts/testpkg/main.sh
+++ home/shellScripts/testpkg/main.sh
@@ -1,4 +1,4 @@
-#/usr/bin/env sh
-which nix-shell
-nix-shell -p "$0" --run fish
-# (which nix-shell) -p "$0" --run fish
+#!/usr/bin/env sh
+
+# Run nix-shell with the current package name
+exec nix-shell -p "$0" --run fish
