f6245dfe153df93544045fd3b1fa381b610ae2b8
Your Name <you@example.com>
2026-01-10 12:02:48 -0500

    a

--- home/shellScripts/githubNotifications/main.sh
+++ home/shellScripts/githubNotifications/main.sh
@@ -4,11 +4,11 @@
 if [ -f /etc/mysecrets/github_token.env ]; then
   source /etc/mysecrets/github_token.env
 else
-  echo "Token missing"
+  echo "Token missing" >&2
   exit 1
 fi
 
-# Fetch unread notifications
+# Fetch unread notifications from GitHub
 curl -s -H "Authorization: token $GITHUB_TOKEN" \
-  https://api.github.com/notifications?all=false|jq 'map({ updated_at: .updated_at, title: .subject.title, url: .subject.url })'
-
+  "https://api.github.com/notifications?all=false" | \
+  jq 'map({ updated_at: .updated_at, title: .subject.title, url: .subject.url })'
