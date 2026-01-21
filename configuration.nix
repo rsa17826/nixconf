30550d9ba3573f7c2ca3e368fbf49824e0b01b4b
Your Name <you@example.com>
2026-01-20 21:52:03 -0500

    a

--- configuration.nix
+++ configuration.nix
@@ -111,6 +111,13 @@
           spc bspc
         )
 
+        ;; passthrough base layer (REQUIRED)
+        (deflayer base
+          a b c d e f g h i j k l m n o p q r s t u v w x y z
+          spc bspc
+        )
+
+        ;; AHK-style text expansion
         (defseq
           (i n s s t e a d spc) (instead spc)
           (s u j e s t i o n s spc) (suggestions spc)
