5a21b10c2c7ff1567eb69bf677e07a95a50a60e1
Your Name <you@example.com>
2026-01-30 14:38:09 -0500

    a

---
+++ home/conf.nix
@@ -0,0 +1,174 @@
+{ uname, pkgs, ... }:
+{
+  boot.loader.grub = {
+    enable = true;
+    device = "/dev/sda"; # Install GRUB into the MBR
+  };
+  programs.nix-ld.enable = true;
+
+  programs.nix-ld.libraries = with pkgs; [
+    # Core X11
+    xorg.libX11
+    xorg.libXcursor
+    xorg.libXext
+    xorg.libXi
+    xorg.libXinerama
+    xorg.libXrandr
+    xorg.libxcb
+
+    # Input & fonts
+    libxkbcommon
+    fontconfig
+
+    # Wayland (fallback)
+    wayland
+
+    # Graphics
+    libGL
+    vulkan-loader
+  ];
+
+  nix.gc.automatic = true;
+  nix.gc.dates = "daily"; # or "daily"
+
+  # Use latest kernel.
+  boot.kernelPackages = pkgs.linuxPackages_latest;
+
+  security.sudo.extraRules = [
+    {
+      groups = [ "users" ];
+      commands = [
+        {
+          command = "/run/current-system/sw/bin/reboot";
+          options = [ "NOPASSWD" ];
+        }
+        {
+          command = "/run/current-system/sw/bin/nixos-rebuild";
+          options = [ "NOPASSWD" ];
+        }
+        {
+          command = "/etc/profiles/per-user/nyx/bin/githubNotifications";
+          options = [
+            "NOPASSWD"
+            # "NOEXEC"
+          ];
+        }
+      ];
+    }
+  ];
+  environment.etc."mysecrets/github_token.env" = {
+    source = "/etc/mysecrets/github_token.env"; # do not overwrite
+    user = "root";
+    group = "root";
+    mode = "0400"; # root-only read
+    enable = true;
+  };
+  # services.kanata = {
+  #   enable = true;
+
+  #   keyboards.default = {
+  #     devices = [
+  #       "/dev/input/by-path/*-event-kbd"
+  #     ];
+
+  #     config = ''
+  #       (defsrc
+  #         a b c d e f g h i j k l m n o p q r s t u v w x y z
+  #         spc bspc
+  #       )
+
+  #       ;; passthrough base layer (REQUIRED)
+  #       (deflayer base
+  #         a b c d e f g h i j k l m n o p q r s t u v w x y z
+  #         spc bspc
+  #       )
+
+  #       ;; AHK-style text expansion
+  #       (defseq
+  #         (i n s s t e a d spc) (instead spc)
+  #         (s u j e s t i o n s spc) (suggestions spc)
+  #         (s u j e s t i o n spc) (suggestion spc)
+  #         (b u t i f i e r spc) (beautifier spc)
+  #         (p r o p i g a t o r spc) (propagator spc)
+  #         (m u n i t e s spc) (minutes spc)
+  #         (m i n i t s spc) (minutes spc)
+  #       )
+  #     '';
+  #   };
+  # };
+  services.keyd = {
+    enable = true;
+    keyboards = {
+      default = {
+        ids = [ "*" ];
+        settings = {
+          main = {
+            capslock = "overload(control, esc)";
+            # numlock = "repeat";
+          };
+        };
+      };
+    };
+  };
+  # systemd.services.numlock = {
+  #   description = "Enable NumLock at startup";
+  #   wantedBy = [ "multi-user.target" ];
+  #   serviceConfig = {
+  #     Type = "oneshot";
+  #     RemainAfterExit = "yes";
+  #     ExecStart = "setleds +num";
+  #   };
+  # };
+  # TODO
+  # might do something
+  # virtualisation.virtualbox.guest.enable = true;
+  console.useXkbConfig = true;
+  fonts.fontconfig = {
+    enable = true;
+    defaultFonts = {
+      sansSerif = [ "JetBrainsMono Nerd Font Propo" ];
+      serif = [ "JetBrainsMono Nerd Font Propo" ];
+      monospace = [ "JetBrainsMono Nerd Font Mono" ];
+      emoji = [ "Noto Color Emoji" ];
+    };
+  };
+
+  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
+  # xdg.portal = {
+  #   enable = true;
+  #   extraPortals = [
+  #     pkgs.xdg-desktop-portal-hyprland
+  #     pkgs.xdg-desktop-portal-gtk # Necessary for fallback
+  #   ];
+  #   config.common.default = "*"; # Or "hyprland;gtk"
+  # };
+  # Configure network proxy if necessary
+  # networking.proxy.default = "http://user:password@proxy:port/";
+  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
+
+  # Enable CUPS to print documents.
+  # services.printing.enable = true;
+
+  # Enable sound with pipewire.
+  services.pulseaudio.enable = false;
+  security.rtkit.enable = true;
+  # TODO
+  # uinput things
+  hardware.uinput.enable = true;
+  programs.mouse-actions.enable = true;
+
+  # Enable touchpad support (enabled default in most desktopManager).
+  # services.xserver.libinput.enable = true;
+
+  # This value determines the NixOS release from which the default
+  # settings for stateful data, like file locations and database versions
+  # on your system were taken. It's perfectly fine and recommended to leave
+  # this value at the release version of the first install of this system.
+  # Before changing this value read the documentation for this option
+  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
+  # sudo codium --no-sandbox --user-data-dir "/home/${uname}/.config/VSCodium/"
+  networking.hostName = "${uname}";
+  system.stateVersion = "25.05"; # Did you read the comment?
+  services.opensnitch.enable = true;
+  security.sudo.enable = true;
+}
