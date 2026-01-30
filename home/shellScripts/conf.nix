6b26c9c6bc9d5c2aacaf0ac56ba6779691aa4b94
Your Name <you@example.com>
2026-01-30 14:09:05 -0500

    a

--- home/shellScripts/conf.nix
+++ home/shellScripts/conf.nix
@@ -1,10 +1,11 @@
 { pkgs, ... }:
 
 let
+  # Define the newsh function
   newsh = { name }: pkgs.writeShellScriptBin name (builtins.readFile (./${name}/main.sh));
 
   # Get all directories that contain a 'main.sh' file
-  scriptDirs = builtins.filter (dir: builtins.isFile (dir + "/main.sh")) (builtins.attrNames ./.);
+  scriptDirs = builtins.filter (dir: builtins.isFile (./${dir}/main.sh)) (builtins.attrNames ./.);
 
   # Generate a list of packages dynamically from these directories
   packages = builtins.map (dir: newsh { name = dir; }) scriptDirs;
