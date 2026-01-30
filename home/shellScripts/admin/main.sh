4b7397d07cbbeb1556d7940d788abdcb8887c0c8
Your Name <you@example.com>
2026-01-30 13:58:33 -0500

    a

--- home/shellScripts/admin/main.sh
+++ home/shellScripts/admin/main.sh
@@ -6,7 +6,6 @@
 admin() {
   # Check if the script is being run as root
   if [ "$(id -u)" -ne 0 ]; then
-    echo "This script requires root privileges. Elevating..."
     # Re-run the calling script with sudo and pass all arguments
     sudo -- "$0" "$@"
     exit  # Exit the current non-root instance after elevation
