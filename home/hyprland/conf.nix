845853d39c28fbf19df1163fb4c79f71180696d0
Your Name <you@example.com>
2026-02-11 13:59:04 -0500

    a

--- home/hyprland/conf.nix
+++ home/hyprland/conf.nix
@@ -14,10 +14,11 @@
   # home.file.".config/hypr/hyprland.conf".source = ./hyprland.conf;
   # home.file.".config/hypr/shaders".source = ./shaders;
   # sudo ln -sf /home/nyx/nixconf/home/hyprland/hyprland.conf "$HOME/.config/hypr/hyprland.conf"
-  home.activation.copy-hyprland-settings = ''
-    echo "Linking hyprland settings..."
-    mkdir -p "$HOME/.config/hypr"
-
-    ln -sf ${./hyprland.conf} "$HOME/.config/hypr/hyprland.conf"
-  '';
+  # home.activation.copy-hyprland-settings = ''
+  #   echo "Linking hyprland settings..."
+  #   mkdir -p "$HOME/.config/hypr"
+  #   rm "$HOME/.config/hypr/hyprland.conf"
+  #   ln -sf "${./hyprland.conf}" "$HOME/.config/hypr/hyprland.conf"
+  # '';
+  home.file.".config/hypr/hyprland.conf".source = ./hyprland.conf;
 }
