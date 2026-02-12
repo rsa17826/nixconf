b23ef9f1d0c9b8ea21cf75412503385f46f2f9b3
rsa17826 <rssaromeo@tutamail.com>
2026-02-12 11:13:27 -0500

    Update programs.nix

--- home/programs.nix
+++ home/programs.nix
@@ -14,7 +14,7 @@ let
     nano = "nvim";
     "nix-env" = "echo wrong command";
     clearcache = "nix-collect-garbage";
-    clearallcache = "sudo nix-collect-garbage --delete-older-than 3d";
+    clearallcache = "sudo nix-collect-garbage --delete-older-than 15d";
     worm = "magic-wormhole send";
     hole = "magic-wormhole receive";
     q = "exit";
