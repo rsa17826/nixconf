94430dc047eb2454f9dc9b71b29eda8b8cb057e8
rsa17826 <rssaromeo@tutamail.com>
2026-02-14 14:17:08 -0500

    Update conf.nix

--- home/disko/conf.nix
+++ home/disko/conf.nix
@@ -7,7 +7,7 @@
       type = "gpt";
       partitions = {
         esp = {
-          size = "512M";
+          size = "2G";
           type = "EF00";
           content = {
             type = "filesystem";
