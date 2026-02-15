5f55d09fbaf42356ea13acf83304a575e1c05118
Your Name <you@example.com>
2026-02-15 16:18:19 -0500

    Update config: nyx

--- home/sops/conf.nix
+++ home/sops/conf.nix
@@ -1,17 +1,11 @@
 { uname, ... }:
 {
   sops = {
-    # 1. Point to your private key
     age.keyFile = "/home/${uname}/.config/sops/age/keys.txt";
-
-    # 2. Point to your secrets file (relative to home.nix)
     defaultSopsFile = ../../secrets/secrets.yaml;
-
-    # 3. Define the secrets you want to symlink
     secrets = {
       GITHUB_TOKEN = {
       };
     };
   };
 }
-# age12kl0dmlpcwuw2c6uq5c8dzhyg34cl7r2qzylxzte0ewe8eemz9jqfq4hl8
