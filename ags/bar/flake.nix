d78a57e437ae1a6f78587e57926548d4b1a792c5
Your Name <you@example.com>
2026-02-22 15:57:33 -0500

    NO MESSAGE SET

--- ags/bar/flake.nix
+++ ags/bar/flake.nix
@@ -1,31 +1,80 @@
 {
+  description = "My Awesome Desktop Shell";
+
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
-    astal.url = "github:aylur/astal";
-    ags.url = "github:aylur/ags";
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+
+    astal = {
+      url = "github:aylur/astal";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    ags = {
+      url = "github:aylur/ags";
+      inputs.nixpkgs.follows = "nixpkgs";
+      inputs.astal.follows = "astal";
+    };
+
   };
 
   outputs =
     {
+      self,
       nixpkgs,
       astal,
       ags,
-      ...
     }:
     let
       system = "x86_64-linux";
       pkgs = nixpkgs.legacyPackages.${system};
+      pname = "my-shell";
+      entry = "app.ts";
+
+      astalPackages = with ags.packages.${system}; [
+        io
+        astal4 # or astal3 for gtk3
+        # notifd tray wireplumber
+      ];
+
+      extraPackages = astalPackages ++ [
+        pkgs.libadwaita
+        pkgs.libsoup_3
+      ];
     in
     {
-      devShells.${system}.default = pkgs.mkShell {
-        buildInputs = [
-          # This includes the 'astallib' and type generation tools
-          astal.packages.${system}.default
-          astal.packages.${system}.io
-          astal.packages.${system}.battery
-          astal.packages.${system}.network
-          # Add any other astal libraries you need here
-        ];
+      packages.${system} = {
+        default = pkgs.stdenv.mkDerivation {
+          name = pname;
+          src = ./.;
+
+          nativeBuildInputs = with pkgs; [
+            wrapGAppsHook3
+            gobject-introspection
+            ags.packages.${system}.default
+          ];
+
+          buildInputs = extraPackages ++ [ pkgs.gjs ];
+
+          installPhase = ''
+            runHook preInstall
+
+            mkdir -p $out/bin
+            mkdir -p $out/share
+            cp -r * $out/share
+            ags bundle ${entry} $out/bin/${pname} -d "SRC='$out/share'"
+
+            runHook postInstall
+          '';
+        };
+      };
+
+      devShells.${system} = {
+        default = pkgs.mkShell {
+          buildInputs = [
+            (ags.packages.${system}.default.override {
+              inherit extraPackages;
+            })
+          ];
+        };
       };
     };
 }
