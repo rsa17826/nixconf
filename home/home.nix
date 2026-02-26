e7f78c693054ac956d6e5e9ab9644ef6768965f2
Your Name <you@example.com>
2026-02-26 17:57:49 -0500

    Generation 40 - nyix - 2026_02_26_17_57_49

--- home/home.nix
+++ home/home.nix
@@ -20,7 +20,7 @@
     ./cursors/conf.nix
     ./sops/conf.nix
     ./zsh/plugins.nix
-    ./keepass/conf.nix
+    # ./keepass/conf.nix
   ];
   #wayland.windowManager.hyprland = {
   #  # Whether to enable Hyprland wayland compositor
