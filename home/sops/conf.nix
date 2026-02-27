1c7b2f2740805455862d52fdc08b85952a4ef5c6
Your Name <you@example.com>
2026-02-27 15:12:45 -0500

    Generation 75 - nyix - 2026_02_27_15_12_45

--- home/sops/conf.nix
+++ home/sops/conf.nix
@@ -5,12 +5,8 @@
     defaultSopsFile = ./secrets.yaml;
     secrets = {
       GITHUB_TOKEN = {
-        owner = userConfig.uname;
-        neededForUsers = true;
       };
       copypartyAdmin = {
-        neededForUsers = true;
-        owner = "copyparty";
         restartUnits = [ "home-assistant.service" ];
         path = "/home/${userConfig.uname}/.config/sops-nix/secrets";
       };
