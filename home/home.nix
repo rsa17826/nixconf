557ec3f2613cb704dfcbabd125390db3f095855e
Your Name <you@example.com>
2026-02-28 10:36:10 -0500

    Generation 126 - nyix - 2026_02_28_10_36_10

--- home/home.nix
+++ home/home.nix
@@ -27,6 +27,18 @@
     dataHome = "/home/${userConfig.uname}/.local/share";
     configHome = "/home/${userConfig.uname}/.config";
     cacheHome = "/home/${userConfig.uname}/.cache";
+    desktopEntries.yazi = {
+      name = "Yazi";
+      exec = "kitty -e yazi %u"; # Replace 'kitty' with your terminal (alacritty, foot, etc.)
+      terminal = false;
+      mimeType = [ "inode/directory" ];
+      categories = [
+        "System"
+        "FileTools"
+        "FileManager"
+      ];
+    };
+
     mimeApps = {
       enable = true;
       defaultApplications = {
