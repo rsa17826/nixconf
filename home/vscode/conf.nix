b712e02793a20d75715e260a8c14e753e5af7799
Your Name <you@example.com>
2026-02-28 08:29:58 -0500

    Generation 95 - nyix - 2026_02_28_08_29_58

--- home/vscode/conf.nix
+++ home/vscode/conf.nix
@@ -642,15 +642,15 @@ in
         ];
     };
   };
-  home.file."VSCodium/User/settings.json".source = ln ./settings.json;
-  home.file."VSCodium/User/keybindings.json".source = ln ./keybindings.json;
-  # home.activation.copy-vscode-settings = ''
-  #   echo "Copying VSCode settings..."
-  #   mkdir -p "$HOME/.config/VSCodium/User"
-  #   cp -f ${./settings.json} "$HOME/.config/VSCodium/User/settings.json"
-  #   cp -f ${./keybindings.json} "$HOME/.config/VSCodium/User/keybindings.json"
-  #   sed -ri 's/\$\{userConfig.uname\}/${userConfig.uname}/g' "$HOME/.config/VSCodium/User/settings.json"
-  # '';
+  # home.file."VSCodium/User/settings.json".source = ln ./settings.json;
+  # home.file."VSCodium/User/keybindings.json".source = ln ./keybindings.json;
+  
+  home.activation.copy-vscode-settings = ''
+    echo "Copying VSCode settings..."
+    mkdir -p "$HOME/.config/VSCodium/User"
+    ln ${./settings.json} "$HOME/.config/VSCodium/User/settings.json"
+    ln ${./keybindings.json} "$HOME/.config/VSCodium/User/keybindings.json"
+  '';
   # language jsonc
   home.file.".vscode-oss/argv.json".text = ''
     {
