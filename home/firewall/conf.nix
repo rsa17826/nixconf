1221daf999d96df79ddf0f443a421f97285d56f5
Your Name <you@example.com>
2026-02-28 10:44:46 -0500

    Generation 128 - nyix - 2026_02_28_10_44_46

--- home/firewall/conf.nix
+++ home/firewall/conf.nix
@@ -1,4 +1,10 @@
 { ... }:
 {
-  networking.firewall.allowedTCPPorts = [ 8086 ];
+  networking.firewall.allowedTCPPorts = [
+    8086
+    1234
+  ];
+  networking.firewall.allowedUDPPorts = [
+    1234
+  ];
 }
