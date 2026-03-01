bea081a70ed0c0afe9a86b3fee7f12446c76fa5b
Your Name <you@example.com>
2026-03-01 13:18:05 -0500

    Generation 180 - nyix - 2026_03_01_13_18_04

--- home/cursors/conf.nix
+++ home/cursors/conf.nix
@@ -3,28 +3,55 @@
   ...
 }:
 {
+  # home.pointerCursor = {
+  #   name = "mew"; # The name of the cursor theme
+  #   size = 24; # Default cursor size (you can adjust this)
+  #   gtk.enable = true;
+  #   x11.enable = true;
+  #   enable = true;
+
+  #   package = pkgs.runCommand "mew" { } ''
+  #       mkdir -p $out/share/icons/mew
+  #       cp -r ${./cursorImages} $out/share/icons/mew/cursors
+  #       cat > $out/share/icons/mew/index.theme <<EOF
+  #     [Icon Theme]
+  #     Name=mew
+  #     Comment=mew
+  #     Hidden=false
+  #     Directories=cursors
+  #     Inherits=Adwaita
+  #     Example=default
+  #     EOF
+
+  #       # Fix permissions just in case
+  #       chmod -R +r $out/share/icons/mew
+  #   '';
+  # };
   home.pointerCursor = {
-    name = "mew"; # The name of the cursor theme
-    size = 48; # Default cursor size (you can adjust this)
+    name = "mew";
+    size = 48;
     gtk.enable = true;
     x11.enable = true;
     enable = true;
 
-    package = pkgs.runCommand "mew" { } ''
-        mkdir -p $out/share/icons/mew
-        cp -r ${./cursorImages} $out/share/icons/mew/cursors
-        cat > $out/share/icons/mew/index.theme <<EOF
+    package = pkgs.runCommand "mew-cursor" { } ''
+            install -dm755 $out/share/icons/mew/cursors
+            
+            # Copy the contents of your folder directly into the cursors dir
+            cp -rn ${./cursorImages}/* $out/share/icons/mew/cursors/
+            
+            # Crucial: Wayland/X11 needs a 'default' file in the cursors dir
+            # Adjust 'left_ptr' to whatever your main pointer file is named in cursorImages
+            if [ -f $out/share/icons/mew/cursors/left_ptr ]; then
+              ln -s left_ptr $out/share/icons/mew/cursors/default
+            fi
+
+            cat > $out/share/icons/mew/index.theme <<EOF
       [Icon Theme]
       Name=mew
       Comment=mew
-      Hidden=false
-      Directories=cursors
       Inherits=Adwaita
-      Example=default
       EOF
-
-        # Fix permissions just in case
-        chmod -R +r $out/share/icons/mew
     '';
   };
 
