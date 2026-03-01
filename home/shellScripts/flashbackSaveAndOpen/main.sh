c86cb61dc08fbb7d5242c7cc5d158661b2916fdb
Your Name <you@example.com>
2026-03-01 17:40:15 -0500

    Generation 193 - nyix - 2026_03_01_17_40_15

--- home/shellScripts/flashbackSaveAndOpen/main.sh
+++ home/shellScripts/flashbackSaveAndOpen/main.sh
@@ -1,14 +1,14 @@
 #!/usr/bin/env bash
 
 # 1. Capture the 'before' state
-LAST_VIDEO=$(ls -t ~/Videos/*.mp4 2>/dev/null | head -1)
+LAST_VIDEO=$(ls -t ~/Videos/flashback/*.mp4 2>/dev/null | head -1)
 
 # 2. Tell the recorder to save
 killall -SIGUSR1 gpu-screen-recorder
 
 # 3. Loop until the newest file is different from the 'before' state
 while true; do
-  CURRENT_VIDEO=$(ls -t ~/Videos/*.mp4 2>/dev/null | head -1)
+  CURRENT_VIDEO=$(ls -t ~/Videos/flashback/*.mp4 2>/dev/null | head -1)
   echo "CURRENT_VIDEO $CURRENT_VIDEO"
   echo "LAST_VIDEO $LAST_VIDEO"
   if [ "$CURRENT_VIDEO" != "$LAST_VIDEO" ] && [ -f "$CURRENT_VIDEO" ]; then
