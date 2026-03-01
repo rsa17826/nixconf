4f24821d5b5734ade4bea424ee31a173965d4e82
Your Name <you@example.com>
2026-03-01 12:09:44 -0500

    Generation 160 - nyix - 2026_03_01_12_09_44

--- home/zsh/plugins.nix
+++ home/zsh/plugins.nix
@@ -32,5 +32,10 @@ in
       src = pkgs.${name};
       file = if (path != null) then path else "share/${name}/${name}.zsh";
     }) zshPlugins;
+    initExtra = ''
+      ZSH_COMMAND_TIME_COLOR="yellow"
+      ZSH_COMMAND_TIME_MIN_SECONDS=3
+      ZSH_COMMAND_TIME_ECHO=1
+    '';
   };
 }
