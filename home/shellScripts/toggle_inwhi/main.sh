2a0f66cc12ce7e17b6cf586d486f499e2e6e886c
Your Name <you@example.com>
2026-03-02 17:16:38 -0500

    Generation 208 - nyix - 2026_03_02_17_16_38

--- home/shellScripts/toggle_inwhi/main.sh
+++ home/shellScripts/toggle_inwhi/main.sh
@@ -6,7 +6,7 @@ SHADER_PATH="~/.config/hypr/shaders/inwhi.glsl"
 # Check if the shader is currently applied
 CURRENT_SHADER=$(hyprctl getoption decoration:screen_shader -j | jq .set)
 echo "$CURRENT_SHADER"
-if [ "$CURRENT_SHADER" ]; then
+if [ "$CURRENT_SHADER" == "true" ]; then
   # If the shader is already applied, disable it
   hyprctl keyword decoration:screen_shader ""
 else
