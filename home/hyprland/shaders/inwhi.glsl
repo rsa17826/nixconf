2890edbf42ebd7e960d6630eeea7d180b1694c4c
Your Name <you@example.com>
2026-03-02 18:02:48 -0500

    Generation 212 - nyix - 2026_03_02_18_02_47

--- home/hyprland/shaders/inwhi.glsl
+++ home/hyprland/shaders/inwhi.glsl
@@ -6,16 +6,19 @@ varying vec2 v_texcoord;
 float rerange(float val, float l1, float h1, float l2, float h2){
     return ((val - l1) / (h1 - l1)) * (h2 - l2) + l2;
 }
-
+float mapToDarkeningFactor(float x) {
+  // return rerange(x, .4, 1, .8,.0);
+  // return rerange(x, 0.5, 0.9, 0.3, 0.0)+.2;
+  return rerange(x, 0.5, 1, 0.8, 0.15);
+  // return rerange(x, 0.5, 0.9, 0.3, 0.0)+.3;
+}
 void main() {
-    // No flipping of Y-axis
-    vec3 col = texture2D(u_tex, v_texcoord).rgb;
+  vec3 color = texture(Source, vTexCoord).rgb;
 
     float avg = (col.r + col.g + col.b) / 3.0;
 
-    float factor = rerange(clamp(avg, 0.5, 1.0), 0.5, 1.0, 0.8, 0.15);
-    if(avg < 0.5)
-        factor = 0.8;
+  float averageIntensity = (color.r + color.g + color.b) / 3.0;
+  float darkeningFactor = mapToDarkeningFactor(averageIntensity);
 
     gl_FragColor = vec4(col * factor, 1.0);
 }
