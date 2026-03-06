aa91bf5481d760f73450293140251f0ef70acc92
Your Name <you@example.com>
2026-03-05 20:50:36 -0500

    Generation 311 - nyix - 2026_03_05_20_50_36

--- home/vscode/extensions/githubAndLocal.nix
+++ home/vscode/extensions/githubAndLocal.nix
@@ -167,7 +167,7 @@ in
         version = "6.0.0";
 
         ghRev = "021eaba326486ac6d82aaa5392a402efde0d053d";
-        ghSha = "sha256-aDZ7HHMdFLwaG6Y2trInJGFAyz+xv/CrSyBBeoZ4Q28=";
+        # ghSha = "sha256-aDZ7HHMdFLwaG6Y2trInJGFAyz+xv/CrSyBBeoZ4Q28=";
         npmDepsHash = "sha256-wWpLlndJnrub7QVskc+jKACETjwv0niVwr6AZjFl1jU=";
         nativeBuildInputs = with pkgs; [
           pkg-config
