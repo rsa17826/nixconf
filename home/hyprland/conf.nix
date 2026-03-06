9b74b4e5761dfd3b3a1c7571cb4fd8125e7cb69a
Your Name <you@example.com>
2026-03-06 15:48:02 -0500

    Generation 332 - nyix - 2026_03_06_15_48_02

--- home/hyprland/conf.nix
+++ home/hyprland/conf.nix
@@ -25,6 +25,7 @@
     };
   };
   wayland.windowManager.hyprland = {
+    generateConfig = false;
     enable = true;
     plugins = [ inputs.hypr-dynamic-cursors.packages.${pkgs.system}.hypr-dynamic-cursors ];
   };
