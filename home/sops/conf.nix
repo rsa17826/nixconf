eb177900e9cd2a0282db7b75feb086a0a1c7fc94
Your Name <you@example.com>
2026-03-23 19:13:30 -0400

    Generation 412 - nyix - 2026_03_23_19_13_30

--- home/sops/conf.nix
+++ home/sops/conf.nix
@@ -9,7 +9,12 @@
       copypartyAdmin = {
         reloadUnits = [ "copyparty.service" ];
         path = "/home/${userConfig.uname}/.config/sops-nix/secrets";
-        owner = userConfig.uname;
+        # owner = userConfig.uname;
+        # 3. Set the group to 'copyparty' so the service can read it
+        group = "copyparty";
+
+        # 4. Set permissions: Read for owner (root) and group (copyparty)
+        mode = "0440";
       };
     };
   };
