2091da0ddb08bb0ea52090ae268802b9e8511f7e
Your Name <you@example.com>
2026-03-23 20:18:13 -0400

    Generation 426 - nyix - 2026_03_23_20_18_12

--- home/brave/conf.nix
+++ home/brave/conf.nix
@@ -32,7 +32,6 @@ in
       (mkCrx {
         name = "dearrow";
         src = "/home/${userConfig.uname}/chrome extensions/dearrow";
-        crxPath = "/home/share/extension.crx";
         version = "2.1.11";
       })
     ];
