0c0301de19aeb1581b7d8447464d6cc921383ca6
Your Name <you@example.com>
2026-03-29 20:29:52 -0400

    Generation 502 - nyix - 2026_03_29_20_29_52

--- home/tts/main.py
+++ home/tts/main.py
@@ -3,16 +3,19 @@ import subprocess
 from http.server import HTTPServer, BaseHTTPRequestHandler
 from urllib.parse import urlparse, parse_qs
 
+ESPEAK = "/run/current-system/sw/bin/espeak-ng"
+
 
 class Handler(BaseHTTPRequestHandler):
   def do_GET(self):
     q = parse_qs(urlparse(self.path).query)
     text = q.get("t", [""])[0]
-    rate = q.get("r", ["175"])[0]
+    rate = q.get("r", ["350"])[0]
     if text:
-      subprocess.Popen(["espeak-ng", "-s", rate, text])
+      subprocess.Popen([ESPEAK, "-s", rate, text])
     self.send_response(200)
     self.send_header("Access-Control-Allow-Origin", "*")
+    self.send_header("Content-Length", "0")
     self.end_headers()
 
   def log_message(self, *a):
