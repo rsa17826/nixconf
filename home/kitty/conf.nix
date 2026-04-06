345cef4c910970618b488e28529f86655ae36e86
Your Name <you@example.com>
2026-04-06 18:13:59 -0400

    Generation 592 - nyix - 2026_04_06_18_13_59

--- home/kitty/conf.nix
+++ home/kitty/conf.nix
@@ -12,21 +12,19 @@
       # "ctrl+shift+right" = "send_text all \\x1b[1;6C";
 
       # Deletion (Ctrl + Backspace / Delete)
-      "ctrl+backspace" = "send_text all \\x17";
-      "ctrl+delete" = "send_text all \\x1bd";
-
+      # "ctrl+backspace" = "send_text all \\x17";
+      # "ctrl+delete" = "send_text all \\x1bd";
       # Optional: Rebind Interrupt so you can still stop processes
-      "ctrl+alt+c" = "send_text all \\x03";
       "ctrl+c" = "copy_and_clear_or_interrupt";
       "ctrl+v" = "paste_from_clipboard";
-      "ctrl+left" = "send_text all \\x1bb";
-      "ctrl+right" = "send_text all \\x1bf";
+      # "ctrl+left" = "send_text all \\x1bb";
+      # "ctrl+right" = "send_text all \\x1bf";
 
-      # Selection (Ctrl + Shift + Left/Right)
-      # These sequences are often interpreted by the shell as movement
-      # unless the application (like a text editor) is listening for selection.
-      "ctrl+shift+left" = "send_text all \\x1b[1;6D";
-      "ctrl+shift+right" = "send_text all \\x1b[1;6C";
+      # # Selection (Ctrl + Shift + Left/Right)
+      # # These sequences are often interpreted by the shell as movement
+      # # unless the application (like a text editor) is listening for selection.
+      # "ctrl+shift+left" = "send_text all \\x1b[1;6D";
+      # "ctrl+shift+right" = "send_text all \\x1b[1;6C";
       "allow_remote_control" = "yes";
     };
   };
