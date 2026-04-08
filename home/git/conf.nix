829e08b3261abb38baef936529a972864c6fd7f3
Your Name <you@example.com>
2026-04-08 16:06:25 -0400

    NO MESSAGE SET

--- home/git/conf.nix
+++ home/git/conf.nix
@@ -7,7 +7,7 @@
         email = userConfig.email;
       };
       credential = {
-        helper = "!${pkgs.gh}/bin/gh auth git-credential";
+        helper = "gh auth git-credential";
       };
     };
     enable = true;
