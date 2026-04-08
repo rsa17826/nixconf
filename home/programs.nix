b74ec47e204e09dbf935fdf7e77410de7fa42cc8
Your Name <you@example.com>
2026-04-08 07:43:58 -0400

    Generation 606 - nyix - 2026_04_08_07_43_57

--- home/programs.nix
+++ home/programs.nix
@@ -229,6 +229,14 @@ in
   };
   programs.gpu-screen-recorder.enable = true;
   environment.systemPackages = with pkgs; [
+    (makeDesktopItem {
+      name = "kitty-audd";
+      desktopName = "Kitty AudD";
+      exec = "kitty sh -c \"audd && exit\"";
+      icon = "utilities-terminal";
+      type = "Application";
+      categories = [ "Utility" ];
+    })
     # nix-direnv
     espeak-ng # tts
     speechd # tts
