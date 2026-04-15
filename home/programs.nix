987e38dd8f27bb2834f2c6f9b983abee4707cf2b
nyix <rsa17826@email.vccs.edu>
2026-04-15 18:52:26 -0400

    Generation 671 - nyix - 2026_04_15_18_52_26

--- home/programs.nix
+++ home/programs.nix
@@ -167,7 +167,9 @@ in
       javaPackages.compiler.temurin-bin.jre-25 # for running java apps
       file # like die
       # opensnitch-ui # firewall
-      vscodium # text editor
+      (vscodium.override {
+        commandLineArgs = "--password-store=basic --enable-blink-features=MiddleClickAutoscroll";
+      }) # text editor
       wineWow64Packages.unstableFull # windows apps
       winetricks
       unixtools.watch # watch cmd
