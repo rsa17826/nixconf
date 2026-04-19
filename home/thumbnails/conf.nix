107ba6c50e677500417010fe59ecb13e7e13c625
nyix <rsa17826@email.vccs.edu>
2026-04-18 21:12:14 -0400

    Generation 726 - nyix - 2026_04_18_21_12_14

--- home/thumbnails/conf.nix
+++ home/thumbnails/conf.nix
@@ -2,38 +2,39 @@
 let
   folderScript = pkgs.writeShellApplication {
     name = "folder-thumbnailer";
-    runtimeInputs = with pkgs; [
-      exiv2
-      imagemagick
-    ];
+    runtimeInputs = with pkgs; [ exiv2 imagemagick ];
     text = ''
-      #!${pkgs.bash}/bin/bash
-      #!/usr/bin/env bash
-      magick -thumbnail "$1" "$2/folder.png" "$3" 1>/dev/null 2>&1 ||\
-      magick -thumbnail "$1" "$2/.folder.png" "$3" 1>/dev/null 2>&1 ||\
-      rm -f "$HOME/.cache/thumbnails/normal/$(echo -n "$4" | md5sum | cut -d " " -f1).png" ||\
-      rm -f "$HOME/.thumbnails/normal/$(echo -n "$4" | md5sum | cut -d " " -f1).png" ||\
-      rm -f "$HOME/.cache/thumbnails/large/$(echo -n "$4" | md5sum | cut -d " " -f1).png" ||\
-      rm -f "$HOME/.thumbnails/large/$(echo -n "$4" | md5sum | cut -d " " -f1).png" ||\
+      magick -thumbnail "$1" "$2/folder.png"  "$3" 2>/dev/null && exit 0
+      magick -thumbnail "$1" "$2/.folder.png" "$3" 2>/dev/null && exit 0
+      rm -f \
+        "$HOME/.cache/thumbnails/normal/$(printf '%s' "$4" | md5sum | cut -d ' ' -f1).png" \
+        "$HOME/.thumbnails/normal/$(printf '%s' "$4" | md5sum | cut -d ' ' -f1).png" \
+        "$HOME/.cache/thumbnails/large/$(printf '%s' "$4" | md5sum | cut -d ' ' -f1).png" \
+        "$HOME/.thumbnails/large/$(printf '%s' "$4" | md5sum | cut -d ' ' -f1).png"
       exit 1
     '';
   };
+
+  folderThumbnailer = pkgs.symlinkJoin {
+    name = "folder-thumbnailer";
+    paths = [
+      folderScript
+      (pkgs.writeTextFile {
+        name = "folder-thumbnailer-thumbnailer";
+        destination = "/share/thumbnailers/folder-thumbnailer.thumbnailer";
+        text = ''
+          [Thumbnailer Entry]
+          Version=1.0
+          Encoding=UTF-8
+          Type=X-Thumbnailer
+          Name=Folder Thumbnailer
+          MimeType=inode/directory;
+          Exec=${folderScript}/bin/folder-thumbnailer %s %i %o %u
+        '';
+      })
+    ];
+  };
 in
 {
-  environment.systemPackages = [
-    folderScript
-    (pkgs.writeTextFile {
-      name = "folder-thumbnailer.thumbnailer";
-      destination = "/share/thumbnailers/folder-thumbnailer.thumbnailer";
-      text = ''
-        [Thumbnailer Entry]
-        Version=1.0
-        Encoding=UTF-8
-        Type=X-Thumbnailer
-        Name=Folder Thumbnailer
-        MimeType=inode/directory;
-        Exec=${folderScript}/bin/folder-thumbnailer %s %i %o %u
-      '';
-    })
-  ];
+  environment.systemPackages = [ folderThumbnailer ];
 }
