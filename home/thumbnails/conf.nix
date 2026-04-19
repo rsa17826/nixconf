75308f4e2639a2d0a885886fd0aadc349364a282
nyix <rsa17826@email.vccs.edu>
2026-04-18 21:09:30 -0400

    Generation 725 - nyix - 2026_04_18_21_09_30

--- home/thumbnails/conf.nix
+++ home/thumbnails/conf.nix
@@ -1,25 +1,27 @@
 { pkgs, ... }:
+let
+  folderScript = pkgs.writeShellApplication {
+    name = "folder-thumbnailer";
+    runtimeInputs = with pkgs; [
+      exiv2
+      imagemagick
+    ];
+    text = ''
+      #!${pkgs.bash}/bin/bash
+      #!/usr/bin/env bash
+      magick -thumbnail "$1" "$2/folder.png" "$3" 1>/dev/null 2>&1 ||\
+      magick -thumbnail "$1" "$2/.folder.png" "$3" 1>/dev/null 2>&1 ||\
+      rm -f "$HOME/.cache/thumbnails/normal/$(echo -n "$4" | md5sum | cut -d " " -f1).png" ||\
+      rm -f "$HOME/.thumbnails/normal/$(echo -n "$4" | md5sum | cut -d " " -f1).png" ||\
+      rm -f "$HOME/.cache/thumbnails/large/$(echo -n "$4" | md5sum | cut -d " " -f1).png" ||\
+      rm -f "$HOME/.thumbnails/large/$(echo -n "$4" | md5sum | cut -d " " -f1).png" ||\
+      exit 1
+    '';
+  };
+in
 {
   environment.systemPackages = [
-    (pkgs.writeShellApplication {
-      name = "folder-thumbnailer";
-      runtimeInputs = with pkgs; [
-        exiv2
-        imagemagick
-      ];
-      text = ''
-        #!${pkgs.bash}/bin/bash
-        #!/usr/bin/env bash
-        magick -thumbnail "$1" "$2/folder.png" "$3" 1>/dev/null 2>&1 ||\
-        magick -thumbnail "$1" "$2/.folder.png" "$3" 1>/dev/null 2>&1 ||\
-        rm -f "$HOME/.cache/thumbnails/normal/$(echo -n "$4" | md5sum | cut -d " " -f1).png" ||\
-        rm -f "$HOME/.thumbnails/normal/$(echo -n "$4" | md5sum | cut -d " " -f1).png" ||\
-        rm -f "$HOME/.cache/thumbnails/large/$(echo -n "$4" | md5sum | cut -d " " -f1).png" ||\
-        rm -f "$HOME/.thumbnails/large/$(echo -n "$4" | md5sum | cut -d " " -f1).png" ||\
-        exit 1
-      '';
-    })
-
+    folderScript
     (pkgs.writeTextFile {
       name = "folder-thumbnailer.thumbnailer";
       destination = "/share/thumbnailers/folder-thumbnailer.thumbnailer";
@@ -30,7 +32,7 @@
         Type=X-Thumbnailer
         Name=Folder Thumbnailer
         MimeType=inode/directory;
-        Exec=/usr/bin/folder-thumbnailer %s %i %o %u
+        Exec=${folderScript}/bin/folder-thumbnailer %s %i %o %u
       '';
     })
   ];
