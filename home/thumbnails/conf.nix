91d2264a35db18c500e65ee7379447909802faa8
nyix <rsa17826@email.vccs.edu>
2026-04-18 22:07:53 -0400

    Generation 735 - nyix - 2026_04_18_22_07_53

--- home/thumbnails/conf.nix
+++ home/thumbnails/conf.nix
@@ -7,7 +7,23 @@ let
       imagemagick
     ];
     text = ''
-      magick "$2/.foldericon.png" -thumbnail "$1" "$3" 2>/dev/null && exit 0
+      size="$1"
+      dir="$(realpath "$2")"
+      output="$3"
+
+      while true; do
+        if [[ "$dir" == "/" ]]; then
+          exit 1
+        fi
+
+        if [[ -f "$dir/.foldericon.png" ]]; then
+          magick "$dir/.foldericon.png" -thumbnail "$size" "$output" 2>/dev/null
+          exit 0
+        fi
+
+        # Move one directory up
+        dir="$(dirname "$dir")"
+      done
       # rm -f \
       #   "$HOME/.cache/thumbnails/normal/$(printf '%s' "$4" | md5sum | cut -d ' ' -f1).png" \
       #   "$HOME/.thumbnails/normal/$(printf '%s' "$4" | md5sum | cut -d ' ' -f1).png" \
