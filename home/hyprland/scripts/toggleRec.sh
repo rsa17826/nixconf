0a83ec6555b96bd4ac9a630fbe1dbeafe7a5013e
nyix <rsa17826@email.vccs.edu>
2026-04-20 17:54:37 -0400

    Generation 744 - nyix - 2026_04_20_17_54_37

--- home/hyprland/scripts/toggleRec.sh
+++ home/hyprland/scripts/toggleRec.sh
@@ -42,5 +42,6 @@ else
   notify-send -e -t 1000 "Recorder" "Recording started: $USER_FILENAME"
 
   # 5. Launch recorder and store its PID
-  gpu-screen-recorder -w region -region "$GEOM" -f 60 -a "default_output" -o "$FINAL_PATH"
+  gpu-screen-recorder -w region -region "$GEOM" -f 60 -a "default_output" -o "$FINAL_PATH" &
+  echo $! >"$PID_FILE"
 fi
