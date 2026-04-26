dca646ac2c1a8b7cff07115d3bffdd0dc76ae369
nyix <rsa17826@email.vccs.edu>
2026-04-26 10:19:20 -0400

    Generation 814 - nyix - 2026_04_26_10_19_20

--- home/shellScripts/goldberg/main.sh
+++ home/shellScripts/goldberg/main.sh
@@ -1,3 +1,9 @@
-#!/usr/bin/env sh
-f=$(realpath "$1")
-"/home/nyix/goldberg emu/steamclient_loader/steamclient_loader.sh" -exe "$f" -appid "$2" -cwd "$(dirname "$f")"
+#!/usr/bin/env bash
+if [[ -n "$1" ]]; then
+  f=$(realpath "$1")
+  echo "$f" >"$(dirname "$f")/ldr_exe.txt"
+  echo "$2" >"$(dirname "$f")/ldr_appid.txt"
+  dirname "$f" >"$(dirname "$f")/ldr_cwd.txt"
+fi
+# "/home/nyix/goldberg emu/steamclient_loader/steamclient_loader.sh" -exe "$f" -appid "$2" -cwd "$(dirname "$f")"
+"/home/nyix/goldberg emu/steamclient_loader/steamclient_loader.sh"
