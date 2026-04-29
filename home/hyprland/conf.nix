cd2f01ff638e3526fd98cac31f502738ec2ca08d
nyix <rsa17826@email.vccs.edu>
2026-04-29 07:31:06 -0400

    Generation 820 - nyix - 2026_04_29_07_31_06

--- home/hyprland/conf.nix
+++ home/hyprland/conf.nix
@@ -43,6 +43,11 @@
   services.hyprpaper = {
     enable = true;
   };
+  security.pam.services.hyprlock = {
+    text = ''
+      auth include login
+    '';
+  };
   # xdg.configFile."hypr/hm.conf".text = ''
   #   plugin = hypr-darkwindow.packages.${pkgs.stdenv.hostPlatform.system}.Hypr-DarkWindow
   # '';
