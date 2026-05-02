09e4f3f9b69bdc78a92f8b5d6a9b16cd1a89b732
nyix <rsa17826@email.vccs.edu>
2026-05-02 09:59:32 -0400

    NO MESSAGE SET

--- home/hyprland/scripts/audacity-kill-dialog.sh
+++ home/hyprland/scripts/audacity-kill-dialog.sh
@@ -2,12 +2,21 @@
 
 SOCKET="$XDG_RUNTIME_DIR/hypr/$HYPRLAND_INSTANCE_SIGNATURE/.socket2.sock"
 
-socat -U - UNIX-CONNECT:"$SOCKET" |
-  while read -r line; do
-    case "$line" in
-    openwindow*save\ changes* | openwindow*Save\ changes*)
-      pkill -9 audacity
-      rm -f /var/tmp/audacity-nyix/*.aup3unsaved*
-      ;;
-    esac
-  done
+# Wait until the socket is actually available
+while [[ ! -S "$SOCKET" ]]; do
+  sleep 0.5
+done
+
+# Restart socat if it ever dies (e.g. Hyprland reloads)
+while true; do
+  socat -U - UNIX-CONNECT:"$SOCKET" |
+    while read -r line; do
+      case "$line" in
+      openwindow*save\ changes* | openwindow*Save\ changes*)
+        pkill -9 audacity
+        rm -f /var/tmp/audacity-nyix/*.aup3unsaved*
+        ;;
+      esac
+    done
+  sleep 1
+done
