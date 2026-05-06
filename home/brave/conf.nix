378e220ee6b2c6c35b0a2990a97ed12432a16fd7
nyix <rsa17826@email.vccs.edu>
2026-05-06 12:17:43 -0400

    Generation 906 - nyix - 2026_05_06_12_17_43

--- home/brave/conf.nix
+++ home/brave/conf.nix
@@ -1,82 +1,30 @@
-{ pkgs, ... }:
-
-let
-  # The standard Chrome Web Store update URL
-  storeUrl = "https://clients2.google.com/service/update2/crx";
-in
+{ lib, ... }:
 {
-  # 2. Use the Chromium module only for the Policies/Extensions
   programs.chromium = {
     enable = true;
 
     extraOpts = {
       "CommandLineFlagSecurityWarningsEnabled" = false;
 
-      "ExtensionSettings" = {
-        # # DeArrow
-        # "nebjniochfgmgoadjlnelfggcmomgopf" = {
-        #   "installation_mode" = "force_installed";
-        #   "update_url" = "${storeUrl}";
-        # };
-        # # LibRedirect
-        # "oladmjdebphlnjjcnomfhhbfdldiimaf" = {
-        #   "installation_mode" = "force_installed";
-        #   "update_url" = "${storeUrl}";
-        # };
-        # # Stylus
-        # "clngdbkpkpeebahjckkjfobafhncgmne" = {
-        #   "installation_mode" = "force_installed";
-        #   "update_url" = "${storeUrl}";
-        #   "file_url_navigation_allowed" = true;
-        # };
-        # # Violentmonkey
-        # "gindolcalhpefcmpmladkchjkfonglfa" = {
-        #   "installation_mode" = "force_installed";
-        #   "update_url" = "${storeUrl}";
-        #   "file_url_navigation_allowed" = true;
-        # };
-        # # XDM
-        # "baejcnbldekpcbiogipmdeohckapojkf" = {
-        #   "installation_mode" = "force_installed";
-        #   "update_url" = "${storeUrl}";
-        #   "file_url_navigation_allowed" = true;
-        # };
-        # SponsorBlock
-        "mnjggcdmjocbbbhaepdhchncahnbgone" = {
-          "installation_mode" = "force_installed";
-          "update_url" = "${storeUrl}";
-        };
-        # Redirector
-        "ocgpenflpmgnfapjedencafcfakcekcd" = {
-          "installation_mode" = "force_installed";
-          "update_url" = "${storeUrl}";
-        };
-
-        # Generic force-installs for the rest
-        "edibdbjcniadpccecjdfdjjppcpchdlm" = {
-          "installation_mode" = "force_installed";
-          "update_url" = "https://clients2.google.com/service/update2/crx";
-        };
-        "eimadpbcbfnmbkopoojfekhnkhdbieeh" = {
-          "installation_mode" = "force_installed";
-          "update_url" = "https://clients2.google.com/service/update2/crx";
-        };
-        "ejddcgojdblidajhngkogefpkknnebdh" = {
-          "installation_mode" = "force_installed";
-          "update_url" = "https://clients2.google.com/service/update2/crx";
-        };
-        "donbcfbmhbcapadipfkeojnmajbakjdc" = {
-          "installation_mode" = "force_installed";
-          "update_url" = "https://clients2.google.com/service/update2/crx";
-        };
-        "fbeffbjdlemaoicjdapfpikkikjoneco" = {
-          "installation_mode" = "force_installed";
-          "update_url" = "https://clients2.google.com/service/update2/crx";
-        };
-      };
+      "ExtensionSettings" =
+        lib.genAttrs
+          [
+            "mnjggcdmjocbbbhaepdhchncahnbgone"
+            "ocgpenflpmgnfapjedencafcfakcekcd"
+            "icallnadddjmdinamnolclfjanhfoafe"
+            "edibdbjcniadpccecjdfdjjppcpchdlm"
+            "eimadpbcbfnmbkopoojfekhnkhdbieeh"
+            "ejddcgojdblidajhngkogefpkknnebdh"
+            "fbeffbjdlemaoicjdapfpikkikjoneco"
+          ]
+          (k: {
+            installation_mode = "force_installed";
+            update_url = "https://clients2.google.com/service/update2/crx";
+          });
     };
   };
 }
+
 # ~/chrome extensions/dearrow
 # nebjniochfgmgoadjlnelfggcmomgopf
 
