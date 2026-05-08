349390887493db56166254a7d4934bbaefbe4b49
nyix <rsa17826@email.vccs.edu>
2026-05-08 11:38:51 -0400

    Generation 939 - nyix - 2026_05_08_11_38_51

--- home/conf.nix
+++ home/conf.nix
@@ -47,7 +47,7 @@ in
   services.udev.extraRules = ''
     KERNEL=="uinput", GROUP="input", MODE="0660"
     KERNEL=="tty0", GROUP="tty", MODE="0660"
-    ACTION=="add", SUBSYSTEM=="leds", KERNEL=="*::scrolllock", RUN+="${pkgs.coreutils}/bin/chmod 666 /sys/class/leds/%k/brightness"
+    ACTION=="add", SUBSYSTEM=="leds", KERNEL=="*::scrolllock", GROUP="video", MODE="0664"
   '';
   security.pam.services.hyprlock = {
     text = ''
