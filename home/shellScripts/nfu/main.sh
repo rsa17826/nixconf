442033471e5d9f6086880544cbaf9b6d5f2a2970
nyix <rsa17826@email.vccs.edu>
2026-05-15 16:10:03 -0400

    Generation 1040 - nyix - 2026_05_15_16_10_02

--- home/shellScripts/nfu/main.sh
+++ home/shellScripts/nfu/main.sh
@@ -1,7 +1,10 @@
 #!/usr/bin/env sh
-
-nix flake metadata --json | jq -r --rawfile excluded ./flake.lock.lock '
+if [ $# -gt 0 ]; then
+  nix flake update --option access-tokens "github.com=$(gh auth token)" "$@"
+else
+  nix flake metadata --json | jq -r --rawfile excluded ./flake.lock.lock '
   ($excluded | split("\n") | map(select(length > 0))) as $list
   | .locks.nodes.root.inputs | keys[]
   | select(. as $k | $list | index($k) | not)
-' | xargs -d '\n' nix flake update --option access-tokens "github.com=$(gh auth token)" "$@"
+' | xargs -d '\n' nix flake update --option access-tokens "github.com=$(gh auth token)"
+fi
