033fe386ba82e8465cb224c03dc44569428bb1ed
nyix <rsa17826@email.vccs.edu>
2026-05-18 14:07:01 -0400

    SUCCESS: Generation 1073 - nyix - 2026_05_18_14_07_01

--- home/vex++/home.nix
+++ home/vex++/home.nix
@@ -10,6 +10,8 @@
         ln "${userConfig.nixConf}/home/vex++/editorBar.sds";
       "/home/nyix/.local/share/godot/app_userdata/vex/main.sds".source =
         ln "${userConfig.nixConf}/home/vex++/main.sds";
+      "/home/nyix/.local/share/godot/app_userdata/vex/config/cfg".source =
+        ln "${userConfig.nixConf}/home/vex++/config/cfg";
       "/home/nyix/.local/share/godot/app_userdata/vex/main - EDITOR.sds".source =
         ln "${userConfig.nixConf}/home/vex++/main - EDITOR.sds";
     };
