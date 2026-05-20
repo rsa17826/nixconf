f7ed2336e8550facfdbc74bca2f2254d30c86a09
nyix <rsa17826@email.vccs.edu>
2026-05-20 13:01:18 -0400

    FAILED: Generation 1113 - nyix - 2026_05_20_13_01_17

--- home/home.nix
+++ home/home.nix
@@ -12,9 +12,11 @@ let
     gtk-application-prefer-dark-theme = 1;
     gtk-enable-animations = 0;
   };
-  editable = (import ./editconf/editconf.nix { inherit pkgs lib; }) config.myProfile.editableConfigs;
+  # Move the execution of the import here, but do NOT pass config to it yet
+  editconfBuilder = import ./editconf/editconf.nix { inherit pkgs lib; };
 in
 {
+  # ── DECLARE OPTIONS ──────────────────────────────────────────────────
   options = {
     myProfile = {
       editableConfigs = lib.mkOption {
@@ -24,278 +26,216 @@ in
       };
     };
   };
-  home = {
-    file = lib.filterAttrs (n: v: lib.hasPrefix ".local" n) editable.entries;
-    username = userConfig.uname;
-    homeDirectory = "/home/${userConfig.uname}";
-    stateVersion = "26.05";
 
-    sessionVariables = {
-      EDITOR = "nvim";
-      SOPS_EDITOR = "codium --wait";
-      VISUAL = "nvim";
-      HYPRCURSOR_THEME = "mew";
-      QT_STYLE_OVERRIDE = "adwaita-dark";
-      ADW_DISABLE_PORTAL = "0";
-      GTK_THEME = "Adwaita-dark";
-      GSETTINGS_SCHEMA_DIR = "${pkgs.gsettings-desktop-schemas}/share/gsettings-schemas/${pkgs.gsettings-desktop-schemas.name}/glib-2.0/schemas";
-      GTK_USE_PORTAL = "1";
-    };
-    packages = [ editable.editScript ];
-  };
-  _module = {
-    args = {
-      ln = config.lib.file.mkOutOfStoreSymlink;
-    };
-  };
-  xdg = {
-    configFile = lib.filterAttrs (n: v: !lib.hasPrefix ".local" n) editable.entries;
-  };
+  # ── AUTOMATICALLY IMPORT SUB-FILES ───────────────────────────────────
   imports = [
     inputs.sops-nix.homeManagerModules.sops
   ]
   ++ (listDir ./. (p: ./${p}/home.nix));
-  systemd = {
-    user = {
-      services = {
-        swaync = {
-          Service = {
-            Restart = lib.mkForce "no";
-          };
-          Install = {
-            # Also, prevent Home Manager from trying to start them during activation
-            WantedBy = lib.mkForce [ ];
-          };
-        };
-        xdg-desktop-portal-gtk = {
-          Service = {
-            Restart = lib.mkForce "no";
-          };
-          Install = {
-            WantedBy = lib.mkForce [ ];
-          };
-        };
-      };
-    };
-  };
-  services = {
-    swaync = {
-      enable = true;
-    };
-    playerctld = {
-      enable = true;
-    };
-  };
-  xdg = {
-    enable = true;
-    dataHome = "/home/${userConfig.uname}/.local/share";
-    configHome = "/home/${userConfig.uname}/.config";
-    cacheHome = "/home/${userConfig.uname}/.cache";
-    desktopEntries = {
-      kitty-audd = {
-        name = "audd";
-        genericName = "audio download";
-        exec = "kitty sh -c \"audd && exit\"";
-        icon = "utilities-terminal";
-        categories = [ "Utility" ];
-        terminal = false;
-      };
-      kitty-vidd = {
-        name = "vidd";
-        genericName = "video download";
-        exec = "kitty sh -c \"vidd && exit\"";
-        icon = "utilities-terminal";
-        categories = [ "Utility" ];
-        terminal = false;
-      };
-    };
-    # desktopEntries.yazi = {
-    #   name = "Yazi";
-    #   exec = "kitty -e yazi %u"; # Replace 'kitty' with your terminal (alacritty, foot, etc.)
-    #   terminal = false;
-    #   mimeType = [ "inode/directory" ];
-    #   categories = [
-    #     "System"
-    #     "FileTools"
-    #     "FileManager"
-    #   ];
-    # };
 
-    mimeApps = {
-      enable = true;
-      defaultApplications = {
-        "inode/directory" = [ "thunar.desktop" ];
-        # "inode/directory" = [ "yazi.desktop" ];
-      };
-    };
+  # ── EVALUATE CONFIGURATION ───────────────────────────────────────────
+  config =
+    let
+      # Evaluating it down here safely isolates it inside the module execution pass
+      editable = editconfBuilder config.myProfile.editableConfigs;
+    in
+    {
+      home = {
+        file = lib.filterAttrs (n: v: lib.hasPrefix ".local" n) editable.entries;
+        username = userConfig.uname;
+        homeDirectory = "/home/${userConfig.uname}";
+        stateVersion = "26.05";
 
-    portal = {
-      enable = true;
-      extraPortals = with pkgs; [
-        xdg-desktop-portal-gtk
-        xdg-desktop-portal-hyprland
-      ];
-      config = {
-        common = {
-          default = [
-            "hyprland"
-            "gtk"
-          ];
+        sessionVariables = {
+          EDITOR = "nvim";
+          SOPS_EDITOR = "codium --wait";
+          VISUAL = "nvim";
+          HYPRCURSOR_THEME = "mew";
+          QT_STYLE_OVERRIDE = "adwaita-dark";
+          ADW_DISABLE_PORTAL = "0";
+          GTK_THEME = "Adwaita-dark";
+          GSETTINGS_SCHEMA_DIR = "${pkgs.gsettings-desktop-schemas}/share/gsettings-schemas/${pkgs.gsettings-desktop-schemas.name}/glib-2.0/schemas";
+          GTK_USE_PORTAL = "1";
         };
-        vscodium = {
-          "org.freedesktop.impl.portal.FileChooser" = [ "gtk" ];
+        packages = [ editable.editScript ];
+      };
+      _module = {
+        args = {
+          ln = config.lib.file.mkOutOfStoreSymlink;
         };
       };
-
-      # enable = true;
-      # config.common.default = "*";
-      # extraPortals = with pkgs; [
-      #   xdg-desktop-portal-gtk
-      #   xdg-desktop-portal-hyprland
-      # ];
-
-      # config = {
-      #   # common.default = [
-      #   #   "hyprland"
-      #   # ];
-      #   # gtk.default = [
-      #   #   "gtk"
-      #   # ];
-      # };
-    };
-  };
-  programs = {
-    direnv = {
-      enable = true;
-      nix-direnv = {
+      xdg = {
+        configFile = lib.filterAttrs (n: v: !lib.hasPrefix ".local" n) editable.entries;
         enable = true;
-      };
-    };
-    kitty = {
-      enable = true;
-    };
-    home-manager = {
-      enable = true;
-    };
-    anyrun = {
-      enable = true;
-      config = {
-        x = {
-          fraction = 0.5;
+        dataHome = "/home/${userConfig.uname}/.local/share";
+        configHome = "/home/${userConfig.uname}/.config";
+        cacheHome = "/home/${userConfig.uname}/.cache";
+        desktopEntries = {
+          kitty-audd = {
+            name = "audd";
+            genericName = "audio download";
+            exec = "kitty sh -c \"audd && exit\"";
+            icon = "utilities-terminal";
+            categories = [ "Utility" ];
+            terminal = false;
+          };
+          kitty-vidd = {
+            name = "vidd";
+            genericName = "video download";
+            exec = "kitty sh -c \"vidd && exit\"";
+            icon = "utilities-terminal";
+            categories = [ "Utility" ];
+            terminal = false;
+          };
         };
-        y = {
-          fraction = 0.3;
+
+        mimeApps = {
+          enable = true;
+          defaultApplications = {
+            "inode/directory" = [ "thunar.desktop" ];
+          };
         };
-        width = {
-          fraction = 0.3;
+
+        portal = {
+          enable = true;
+          extraPortals = with pkgs; [
+            xdg-desktop-portal-gtk
+            xdg-desktop-portal-hyprland
+          ];
+          config = {
+            common = {
+              default = [
+                "hyprland"
+                "gtk"
+              ];
+            };
+            vscodium = {
+              "org.freedesktop.impl.portal.FileChooser" = [ "gtk" ];
+            };
+          };
         };
-        hideIcons = false;
-        ignoreExclusiveZones = false;
-        layer = "overlay";
-        hidePluginInfo = false;
-        closeOnClick = false;
-        showResultsImmediately = true;
-        maxEntries = null;
-
-        plugins = [
-          "${pkgs.anyrun}/lib/libapplications.so"
-          # "${pkgs.anyrun}/lib/libsymbols.so"
-          #"${pkgs.anyrun}/lib/libshell.so"
-          #"${pkgs.anyrun}/lib/libdictionary.so"
-        ];
       };
 
-      # Inline comments are supported for language injection into
-      # multi-line strings with Treesitter! (Depends on your editor)
-      extraCss = /* css */ ''
-        .some_class {
-          background: red;
-        }
-      '';
+      systemd = {
+        user = {
+          services = {
+            swaync = {
+              Service = {
+                Restart = lib.mkForce "no";
+              };
+              Install = {
+                # Also, prevent Home Manager from trying to start them during activation
+                WantedBy = lib.mkForce [ ];
+              };
+            };
+            xdg-desktop-portal-gtk = {
+              Service = {
+                Restart = lib.mkForce "no";
+              };
+              Install = {
+                WantedBy = lib.mkForce [ ];
+              };
+            };
+          };
+        };
+      };
 
-      extraConfigFiles."some-plugin.ron".text = ''
-        Config(
-          // for any other plugin
-          // this file will be put in ~/.config/anyrun/some-plugin.ron
-          // refer to docs of xdg.configFile for available options
-        )
-      '';
-    };
-  };
+      programs = {
+        direnv = {
+          enable = true;
+          nix-direnv = {
+            enable = true;
+          };
+        };
+        kitty = {
+          enable = true;
+        };
+        home-manager = {
+          enable = true;
+        };
+        anyrun = {
+          enable = true;
+          config = {
+            x = {
+              fraction = 0.5;
+            };
+            y = {
+              fraction = 0.3;
+            };
+            width = {
+              fraction = 0.3;
+            };
+            hideIcons = false;
+            ignoreExclusiveZones = false;
+            layer = "overlay";
+            hidePluginInfo = false;
+            closeOnClick = false;
+            showResultsImmediately = true;
+            maxEntries = null;
 
-  # Optional, hint Electron apps to use Wayland:
-  # home.sessionVariables.NIXOS_OZONE_WL = "1";
+            plugins = [
+              "${pkgs.anyrun}/lib/libapplications.so"
+              # "${pkgs.anyrun}/lib/libsymbols.so"
+              #"${pkgs.anyrun}/lib/libshell.so"
+              #"${pkgs.anyrun}/lib/libdictionary.so"
+            ];
+          };
+          # Inline comments are supported for language injection into
+          # multi-line strings with Treesitter! (Depends on your editor)
+          extraCss = /* css */ ''
+            .some_class {
+              background: red;
+            }
+          '';
 
-  #home.file.".icons/mew".source = lib.mkForce ./cursors;
+          extraConfigFiles."some-plugin.ron".text = ''
+            Config(
+              // for any other plugin
+              // this file will be put in ~/.config/anyrun/some-plugin.ron
+              // refer to docs of xdg.configFile for available options
+            )
+          '';
+        };
+      };
 
-  # Home Manager is pretty good at managing dotfiles. The primary way to manage
-  # plain files is through 'home.file'.
+      gtk = {
+        enable = true;
+        theme = {
+          name = "Adwaita-dark";
+          package = pkgs.gnome-themes-extra;
+        };
+        gtk3 = {
+          extraConfig = gtkExtraConfig;
+        };
+        gtk4 = {
+          extraConfig = gtkExtraConfig;
+          # gtk4.theme = config.gtk.theme;
+          theme = null;
+        };
+      };
 
-  # home.packages = [
-  #   (pkgs.writeShellScriptBin "nix-env" ''
-  #     echo "nix-env is deprecated. Use nix profile or Home Manager."
-  #     exit 1
-  #   '')
-  # ];
-
-  # Home Manager can also manage your environment variables through
-  # 'home.sessionVariables'. These will be explicitly sourced when using a
-  # shell provided by Home Manager. If you don't want to manage your shell
-  # through Home Manager then you have to manually source 'hm-session-vars.sh'
-  # located at either
-  #
-  #  ~/.nix-profile/etc/profile.d/hm-session-vars.sh
-  #
-  # or
-  #
-  #  ~/.local/state/nix/profiles/profile/etc/profile.d/hm-session-vars.sh
-  #
-  # or
-  #
-  #  /etc/profiles/per-user/nyx/etc/profile.d/hm-session-vars.sh
-  #
-  gtk = {
-    enable = true;
-
-    theme = {
-      name = "Adwaita-dark";
-      package = pkgs.gnome-themes-extra;
-    };
-    gtk3 = {
-      extraConfig = gtkExtraConfig;
-    };
-    gtk4 = {
-      extraConfig = gtkExtraConfig;
-      # gtk4.theme = config.gtk.theme;
-      theme = null;
-    };
-  };
-
-  qt = {
-    enable = true;
-    platformTheme = {
-      name = "gtk2";
-    };
-    style = {
-      name = "adwaita-dark";
-      package = pkgs.symlinkJoin {
-        name = "adwaita-qt-all";
-        paths = with pkgs; [
-          adwaita-qt
-          adwaita-qt6
-        ];
+      qt = {
+        enable = true;
+        platformTheme = {
+          name = "gtk2";
+        };
+        style = {
+          name = "adwaita-dark";
+          package = pkgs.symlinkJoin {
+            name = "adwaita-qt-all";
+            paths = with pkgs; [
+              adwaita-qt
+              adwaita-qt6
+            ];
+          };
+        };
+      };
+      dconf = {
+        settings = {
+          "org/gnome/desktop/interface" = {
+            color-scheme = "prefer-dark";
+          };
+        };
       };
     };
-  };
-  dconf = {
-    settings = {
-      "org/gnome/desktop/interface" = {
-        color-scheme = "prefer-dark";
-      };
-    };
-  };
-
-  # Bootloader.
-  # boot.loader.systemd-boot.enable = true;
-  # boot.loader.efi.canTouchEfiVariables = true;
 }
