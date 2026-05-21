68aa2efa2536bf75894a3e641233b67fc69ba1c3
nyix <rsa17826@email.vccs.edu>
2026-05-21 19:26:44 -0400

    ❌ Generation 1152 - nyix - 2026_05_21_19_26_44

--- home/zsh/home.nix
+++ home/zsh/home.nix
@@ -20,9 +20,14 @@ in
     zsh = {
       dotDir = "${config.xdg.configHome}/zsh";
       enable = true;
+
+      # --- 1. REMOVE DUPLICATES FROM HISTORY ---
       history = {
         size = 10000;
+        ignoreAllDups = true; # Ensures a command is only in history once
+        expireDuplicatesFirst = true;
       };
+
       enableCompletion = true;
       autosuggestion = {
         enable = true;
@@ -35,25 +40,58 @@ in
         src = pkgs.${name};
         file = if (path != null) then path else "share/${name}/${name}.zsh";
       }) zshPlugins;
-      initContent = ''
-        ${pkgs.any-nix-shell}/bin/any-nix-shell zsh --info-right | source /dev/stdin
-        ZSH_COMMAND_TIME_COLOR="yellow"
-        ZSH_COMMAND_TIME_MIN_SECONDS=3
-        ZSH_COMMAND_TIME_ECHO=1
-        bindkey "\e[1;5D" backward-word
-        bindkey "\e[1;5C" forward-word
-        bindkey "\e[1;6D" backward-word
-        bindkey "\e[1;6C" forward-word
-        bindkey '^H' backward-kill-word
-        bindkey '^[d' kill-word
-        bindkey "\e[3;5~" kill-word
-        bindkey '^[[A' history-substring-search-up
-        bindkey '^[[B' history-substring-search-down
-        bindkey "$terminfo[kcuu1]" history-substring-search-up
-        bindkey "$terminfo[kcud1]" history-substring-search-down
-        bindkey '^[[Z' reverse-menu-complete
-        setopt INTERACTIVE_COMMENTS
-      '';
+
     };
+    initContent = ''
+      ${pkgs.any-nix-shell}/bin/any-nix-shell zsh --info-right | source /dev/stdin
+
+      ZSH_COMMAND_TIME_COLOR="yellow"
+      ZSH_COMMAND_TIME_MIN_SECONDS=3
+      ZSH_COMMAND_TIME_ECHO=1
+
+      # --- CUSTOM WORD-BOUNDARY HISTORY SEARCH ---
+      custom-history-search() {
+        setopt localoptions extendedglob
+
+        # shellcheck disable=SC2206
+        # Fix: Removed space after (z) and added linter bypass for Zsh array assignment
+        local words=(''${(z)LBUFFER})
+        [[ -z "$LBUFFER" ]] && { zle up-line-or-history; return }
+
+        local pattern=""
+        local word
+        for word in $words; do
+          local escaped_word="''${word//([.\\*^$])/\\\$MATCH}"
+          pattern="$pattern(|.*[;\|&[:space:]])$escaped_word"
+        done
+        pattern="^$pattern*"
+
+        if [[ "$WIDGET" == *down* ]]; then
+          zle .history-beginning-search-forward "$pattern"
+        else
+          zle .history-beginning-search-backward "$pattern"
+        fi
+      }
+
+      zle -N custom-history-search-up custom-history-search
+      zle -N custom-history-search-down custom-history-search
+
+      # --- BINDINGS ---
+      bindkey "\e[1;5D" backward-word
+      bindkey "\e[1;5C" forward-word
+      bindkey "\e[1;6D" backward-word
+      bindkey "\e[1;6C" forward-word
+      bindkey '^H' backward-kill-word
+      bindkey '^[d' kill-word
+      bindkey "\e[3;5~" kill-word
+
+      bindkey '^[[A' custom-history-search-up
+      bindkey '^[[B' custom-history-search-down
+      bindkey "$terminfo[kcuu1]" custom-history-search-up
+      bindkey "$terminfo[kcud1]" custom-history-search-down
+
+      bindkey '^[[Z' reverse-menu-complete
+      setopt INTERACTIVE_COMMENTS
+    '';
   };
 }
