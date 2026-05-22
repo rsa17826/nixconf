f44c377b92d1acbfa9c0f88c335e767845b32bcf
nyix <rsa17826@email.vccs.edu>
2026-05-22 19:41:52 -0400

    ✅ Generation 1211 - nyix - 2026_05_22_19_41_52

--- home/tmux/home.nix
+++ home/tmux/home.nix
@@ -5,7 +5,7 @@
       {
         name = "tmux";
         src = ./.;
-        srcStr = "${userConfig.nixConf}/home";
+        srcStr = "${userConfig.nixConf}/home/tmux";
         nixKey = "tmux";
         dest = "$HOME";
         files = [
