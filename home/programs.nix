e517857601298331d9241b50bb7c2794611143ca
nyix <rsa17826@email.vccs.edu>
2026-05-30 09:24:43 -0400

    ✅ Generation 1303 - nyix - 2026_05_30_09_24_42

--- home/programs.nix
+++ home/programs.nix
@@ -431,7 +431,7 @@ in
       neovim # tui text editor
       wget # cmd dl util
       (brave.override {
-        commandLineArgs = "--remote-debugging-port=9222 --password-store=basic --enable-blink-features=MiddleClickAutoscroll";
+        commandLineArgs = "--remote-debugging-port=9222 --password-store=basic --enable-blink-features=MiddleClickAutoscroll --force-dark-mode";
       }) # web browser
       nixfmt # nix language formatter
       git # git is required
