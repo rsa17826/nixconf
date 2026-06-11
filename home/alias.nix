86a867d2048b52cea7495ea38e16c6094a336d04
nyix <rsa17826@email.vccs.edu>
2026-06-11 18:13:07 -0400

    ✅ Generation 1381 - nyix - 2026_06_11_18_13_06

--- home/alias.nix
+++ home/alias.nix
@@ -25,6 +25,7 @@ let
     updatec = "cd ~/nixconf ; (nix flake metadata | grep -oE 'ext-[a-zA-Z0-9_-]+'|xargs nix flake update) && pkill -9 codium ; update && codium && q";
     dea = "echo 'use flake' > .envrc && echo '.direnv' >> .gitignore && git rm -r --cached .direnv 2>/dev/null; direnv allow";
     "7z" = "7zz";
+    rm = "trash";
     # cd = "z";
   };
   # commonInit = ''
