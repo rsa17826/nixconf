a95956377da6b8b7084b564d8e87bc268cc3db36
nyix <rsa17826@email.vccs.edu>
2026-07-25 10:07:47 -0400

    ❌ Generation 1562 - nyix - 2026_07_25_10_07_46

--- home/programs.nix
+++ home/programs.nix
@@ -3,8 +3,6 @@
   userConfig,
   inputs,
   pkgFromInp,
-  fetchCrate,
-  rustPlatform,
   ...
 }:
 let
@@ -187,31 +185,6 @@ in
           bandwhich
           #
           (pkgFromInp "freenet-core" "default")
-          (rustPlatform.buildRustPackage (finalAttrs: {
-            pname = "freenet-core";
-            version = "0.1.118";
-
-            src = fetchCrate {
-              pname = "freenet";
-              version = finalAttrs.version;
-              hash = "sha256-UGNNZ87thBKZ5XEFLpMtdaTv/wcG+IgU8wnMpovQ5yg=";
-            };
-
-            cargoHash = "sha256-VflN0W1HigyEky6IzQEB88jyCWYsjaXi1VqDowRz8b4=";
-
-            # freenet edge-case tests cause build failure otherwise
-            doCheck = false;
-
-            meta = {
-              description = "Freenet Core";
-              homepage = "https://freenet.org/";
-              license = lib.licenses.agpl3Only;
-              platforms = with lib.platforms; linux;
-              maintainers = [ ];
-              changelog = "https://github.com/freenet/freenet-core/releases/tag/v${finalAttrs.version}";
-              mainProgram = "freenet";
-            };
-          }))
           python313Packages.wsgidav
           steam-run-free
           docker
