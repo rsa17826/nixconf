5d93221edfadde0b08cff952b961b66583814f55
nyix <rsa17826@email.vccs.edu>
2026-08-04 08:50:47 -0400

    ❌ Generation 1648 - nyix - 2026_08_04_08_50_47

--- home/port-redir/conf.nix
+++ home/port-redir/conf.nix
@@ -5,12 +5,53 @@ let
       "timer"
       8765
     ]
+    [
+      "nullserv"
+      7542
+    ]
   ];
+
+  # Generate HTML list items for the index page
+  indexHtml = ''
+    <!DOCTYPE html>
+    <html>
+      <head><title>Service Dashboard</title></head>
+      <body>
+        <h1>Available Services</h1>
+        <ul>
+          ${lib.concatStringsSep "\n" (
+            map (
+              pair:
+              let
+                name = builtins.elemAt pair 0;
+              in
+              "<li><a href=\"http://${name}.127.0.0.1\">${name}</a></li>"
+            ) remaps
+          )}
+        </ul>
+      </body>
+    </html>
+  '';
 in
 {
   services = {
     nginx = {
       enable = true;
+
+      # Define a default virtual host to catch requests to http://127.0.0.1/
+      virtualHosts."_" = {
+        default = true;
+        locations = {
+          "/" = {
+            return = "200 '${indexHtml}'";
+            extraConfig = ''
+              add_header Content-Type text/html;
+            '';
+          };
+        };
+      };
+
+      # Your existing dynamic virtual hosts for the subdomains
       virtualHosts = lib.listToAttrs (
         map (
           pair:
@@ -35,10 +76,6 @@ in
   };
 
   networking = {
-    # firewall.allowedTCPPorts = [
-    #   80
-    #   443
-    # ];
     extraHosts = lib.concatStringsSep "\n" (
       map (pair: "127.0.0.1 ${builtins.elemAt pair 0}.127.0.0.1") remaps
     );
