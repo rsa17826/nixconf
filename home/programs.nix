1a6a4721a40ae2fd769ce5f540718cb66f13fa60
nyix <rsa17826@email.vccs.edu>
2026-08-28 07:47:00 -0400

    ✅ Generation 1826 - nyix - 2026_08_28_07_47_00

--- home/programs.nix
+++ home/programs.nix
@@ -354,7 +354,7 @@ in
           mediamtx
           # (callPackage ./progress-daemon/progress-daemon.nix { })
           (callPackage ./winspy/winspy.nix { })
-          (callPackage ./devfs/devfs.nix { })
+          (callPackage ./webfs/webfs.nix { })
           goldberg-emu
           deno
           zuban
