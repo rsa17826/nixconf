4b9fcbb024b0264ad2378a6d6ad7ac9374a35d5a
nyix <rsa17826@email.vccs.edu>
2026-09-08 19:48:19 -0400

    🛑 Generation 1887 - nyix - 2026_09_08_19_48_18

--- home/hyprland/conf/binds/launcher.lua
+++ home/hyprland/conf/binds/launcher.lua
@@ -29,7 +29,7 @@ hl.define_submap("launcher", function()
 	m.bind(
 		"k",
 		launch(
-			"sh -c 'sys restart input-manager;sys restart autocorrect;sys restart autoclicker;sys restart macro-recorder;sys restart dynamicRebinds'"
+			"sh -c 'sys restart input-manager;sys restart autocorrect&sys restart autoclicker&sys restart macro-recorder&sys restart dynamicRebinds'"
 		)
 	)
 
