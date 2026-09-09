ba1bcfc950368402dad522b4336b1ef2450515ae
nyix <rsa17826@email.vccs.edu>
2026-09-09 10:35:14 -0400

    ❌ Generation 1894 - nyix - 2026_09_09_10_35_14

--- home/programs.nix
+++ home/programs.nix
@@ -291,7 +291,7 @@ in
           python313Packages.black
           ((import (fetchTarball {
             url = "https://github.com/NixOS/nixpkgs/archive/e6f23dc08d3624daab7094b701aa3954923c6bbb.tar.gz";
-            sha256 = "sha256:0m0xmk8sjb5gv2pq7s8w7qxf7qggqsd3rxzv3xrqkhfimy2x7bnx";
+            sha256 = "sha256-3a7Tha/RwYlzH/v3PJrG7+HjOj4c6YOv2K8sqdGsHVQ=";
           }) { }).audacity
           )
           mediamtx
