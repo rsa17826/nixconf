4d600b2961058943ac02e8c9cda94a4e028e94ad
nyix <rsa17826@email.vccs.edu>
2026-09-09 11:50:00 -0400

    Generation 1898 - nyix - 2026_09_09_11_50_00

--- home/programs.nix
+++ home/programs.nix
@@ -290,6 +290,7 @@ in
           adwaita-icon-theme
           python313Packages.black
           # (inputs.audacity.legacyPackages.${pkgs.stdenv.hostPlatform.system}.audacity)
+          tenacity
           mediamtx
           # (callPackage ./progress-daemon/progress-daemon.nix { })
           (callPackage ./winspy/winspy.nix { })
