7cf41ef2948a271e46a476f1a8336d62a1561d34
nyix <rsa17826@email.vccs.edu>
2026-09-09 17:37:09 -0400

    ✅ Generation 1900 - nyix - 2026_09_09_17_37_09

--- home/hyprland/conf/windowrule_requests/req_e341b1a0d721465e.lua
+++ home/hyprland/conf/windowrule_requests/req_e341b1a0d721465e.lua
@@ -10,11 +10,32 @@ hl.window_rule({
 	move = { "0", "0" },
 	border_size = 0,
 })
-hl.window_rule({
-	match = { class = "^TIMER$", tag = "HIDE" },
-	opacity = "0 override",
+local timer_visible = hl.window_rule({
+	name = "timer-visible",
+	match = {
+		class = "^TIMER$",
+	},
+	opacity = "1 override 1 override 1 override",
 })
-hl.window_rule({
-	match = { class = "^TIMER$" },
-	opacity = "1 override",
+
+local timer_hidden = hl.window_rule({
+	name = "timer-hidden",
+	match = {
+		class = "^TIMER$",
+	},
+	opacity = "0 override 0 override 0 override",
 })
+
+-- Start hidden.
+timer_visible:set_enabled(false)
+timer_hidden:set_enabled(true)
+
+hl.on("window.active", function(w)
+	if w.class == "explorer.exe" then
+		timer_hidden:set_enabled(false)
+		timer_visible:set_enabled(true)
+	else
+		timer_visible:set_enabled(false)
+		timer_hidden:set_enabled(true)
+	end
+end)
